1d77a5f4c8684ed4837a22f50bc79f636f5a2a52
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-07 13:40:58 +0330

    creating properties for the app menu

--- app/src/main/java/com/example/forecast/MainActivity.java
+++ app/src/main/java/com/example/forecast/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.forecast;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigation;
+    Toolbar toolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolBar = findViewById(R.id.toolBar);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigation = findViewById(R.id.navigation);
+        setSupportActionBar(toolBar);
+        ActionBarDrawerToggle toggle;
+        toggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolBar,
+                R.string.open,
+                R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
