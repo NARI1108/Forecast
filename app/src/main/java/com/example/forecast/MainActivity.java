086065bdd0993a9fc708761d81b797191cf68239
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-08 21:44:35 +0330

    Making changes on the menu list, on all its options.

--- app/src/main/java/com/example/forecast/MainActivity.java
+++ app/src/main/java/com/example/forecast/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.forecast;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     DrawerLayout drawerLayout;
     NavigationView navigation;
     Toolbar toolBar;
@@ -27,5 +31,39 @@ public class MainActivity extends AppCompatActivity {
                 R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+       navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               int id=menuItem.getItemId();
+               switch(id){
+                   case R.id.home:
+                       Toast.makeText(getApplicationContext(), "home", Toast.LENGTH_SHORT).show();
+                       break;
+                   case R.id.about_Us:
+                       Toast.makeText(getApplicationContext(), "about us", Toast.LENGTH_SHORT).show();
+                       break;
+                   case R.id.help:
+                       Toast.makeText(getApplicationContext(), "help", Toast.LENGTH_SHORT).show();
+                       break;
+                   case R.id.contact:
+                       Toast.makeText(getApplicationContext(), "Contact", Toast.LENGTH_SHORT).show();
+                       break;
+                   case R.id.exit:
+                       finish();
+                       break;
+               }
+               drawerLayout.closeDrawer(GravityCompat.START);
+               return false;
+           }
+       });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+               drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
