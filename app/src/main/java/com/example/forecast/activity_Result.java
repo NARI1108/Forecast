88d0b6b7294f217bdbd789f36f76e39f328dc33a
Hill <125409221+NARI1108@users.noreply.github.com>
2023-03-25 22:01:46 +0430

    creating a the Timer method for result in activity_Result

--- app/src/main/java/com/example/forecast/activity_Result.java
+++ app/src/main/java/com/example/forecast/activity_Result.java
@@ -1,6 +1,7 @@
 package com.example.forecast;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -8,7 +9,8 @@ import android.widget.Toast;
 public class activity_Result extends AppCompatActivity {
     String  res_cities_name,res_name;
     String name_user, age;
-    TextView txt_user,txt_Love,txt_finalRes;
+    TextView txt_user,txt_Love,txt_finalRes,txt_timer;
+    int time=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +18,7 @@ public class activity_Result extends AppCompatActivity {
         txt_user = findViewById(R.id.txt_user);
         txt_Love = findViewById(R.id.txt_Love);
         txt_finalRes = findViewById(R.id.txt_finalRes);
+        txt_timer = findViewById(R.id.txt_timer);
         res_cities_name = getIntent().getStringExtra("res_cities_name");
         res_name = getIntent().getStringExtra("res_name");
         name_user = getIntent().getStringExtra("name");
@@ -23,5 +26,18 @@ public class activity_Result extends AppCompatActivity {
         txt_user.setText(name_user);
         txt_Love.setText(res_name);
         txt_finalRes.setText("اهل "+res_cities_name+" به به بهم میاین چقدر...");
+        timer();
+    }
+    private void timer(){
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                time--;
+                txt_timer.setText("0"+time);
+                if(time>0){
+                    timer();
+                }
+            }
+        }, 1000);
     }
 }
